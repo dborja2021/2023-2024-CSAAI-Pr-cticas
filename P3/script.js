33967419fa46c3aa5c8e13e5d0011d7f38058878
dborja2021 <159265845+dborja2021@users.noreply.github.com>
2024-03-21 10:43:25 +0100

    Add files via upload

--- P3/script.js
+++ P3/script.js
@@ -2,62 +2,61 @@ console.log("Ejecutando JS...");
 
 const canvas = document.getElementById("canvas");
 
+// Captura los elementos de ángulo y velocidad
+const anguloInput = document.getElementById('angulo');
+const velocidadInput = document.getElementById('velocidad');
+
+// Captura los elementos donde mostrar los valores seleccionados
+const anguloValor = document.getElementById('angulo_valor');
+const velocidadValor = document.getElementById('velocidad_valor');
+
 //-- Definir el tamaño del canvas
 canvas.width = 780;
 canvas.height = 460;
 
 // obtener contexto canvas
 const ctx = canvas.getContext("2d");
-/*
-// Dibujar rectangulo
-ctx.beginPath();
-    // Esquina sup. izq. y dimensiones
-    ctx.rect(5,405,50,50);
-    // Dibujar
-    ctx.fillStyle = 'green';
-    ctx.lineWidth = 2;
-    // Relleno
-    ctx.fill();
-    // Trazo
-    ctx.stroke();
-ctx.closePath();
-
-// Dibujar circulo
-ctx.beginPath();
-    // Centro, radio, angulo inicial, angulo final
-    ctx.arc(390, 430, 25, 0, 2*Math.PI);
-    ctx.strokeStyle = 'blue';
-    ctx.lineWidth = 3;
-    ctx.fillStyle = 'red';
-    ctx.stroke();
-    ctx.fill();
-ctx.closePath();
-*/
 
 // Variables para la posicion y velocidad
 let x = 5;
 let y = 405;
 let vx = 0; // Velocidad horzontal
-let = vy = 0 // Velocidad vertical
-let g = 0.25 // Gravedad
+let = vy = 0; // Velocidad vertical
+let g = 0.2; // Gravedad
 
 // Variables para la velocidad y el ángulo de disparo
-let velocidad = 10; // CAMBIAR PARA SELECCIONARLA
-let angulo = Math.PI/4;
+let velocidad = velocidadInput.value; // CAMBIAR PARA SELECCIONARLA
+let angulo = anguloInput.value * Math.PI/180;
 
 let disparar = false;
+let xCircle = 360;
+
+let tiempoVuelo = 0;
+let tiempoInicio = 0;
+
+// Agrega event listener para actualizar los valores cuando cambien los controles de rango
+anguloInput.addEventListener('input', () => {
+    anguloValor.textContent = anguloInput.value;
+    angulo = anguloInput.value * Math.PI/180;
+});
+
+velocidadInput.addEventListener('input', () => {
+    velocidadValor.textContent = velocidadInput.value;
+    velocidad = velocidadInput.value;
+});
 
 // Controlador boton disparo
 document.getElementById("disparo").addEventListener("click", function(){
     // Calcular las velocidades iniciales en las direcciones x e y
     if (disparo){
-        vx = velocidad *Math.cos(angulo);
+        vx = velocidad * Math.cos(angulo);
         vy = -velocidad * Math.sin(angulo);
         disparar = true;
     }
+
 });
 
-// Contrl boton inicio
+// Control boton inicio
 document.getElementById("inicio").addEventListener("click",function(){
     // Reiniciar la posición y velocidad del cuadrado
     x = 5;
@@ -65,6 +64,12 @@ document.getElementById("inicio").addEventListener("click",function(){
     vx = 0;
     vy = 0;
     disparar = false; // Detener el disparo
+
+    tiempoVuelo = 0;
+
+    // Generar nuevas coordenadas para el circulo
+    let rawXCircle = (Math.random()+1) * (300);
+    xCircle = Math.max(200, Math.min(rawXCircle, 780));
 });
 
 // Funcion para actualizar la posicion edl cuadrado
@@ -76,7 +81,7 @@ function update(){
     y += vy;
 
     // Comprobar si el cuadrado ha llegado al borde del lienzo
-    if (x < 0 || x > canvas.width - 50 || y < 0 || y > canvas.height - 50) {
+    if (x < 0 || x > canvas.width - 55 || y < 0 || y > canvas.height - 55) {
         disparar = false; // Detener el disparo
     }
     }
@@ -94,7 +99,7 @@ function update(){
     // Dibujar circulo
     ctx.beginPath();
     // Centro, radio, angulo inicial, angulo final
-    ctx.arc(390, 430, 25, 0, 2*Math.PI);
+    ctx.arc(xCircle, 430, 25, 0, 2*Math.PI);
     ctx.strokeStyle = 'blue';
     ctx.lineWidth = 3;
     ctx.fillStyle = 'red';
@@ -102,9 +107,17 @@ function update(){
     ctx.fill();
     ctx.closePath();
 
+    // Mostrar el tiempo de vuelo
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText("Tiempo de vuelo: " + tiempoVuelo.toFixed(2), 10, 30);
+
     // Solicitar el próximo cuadro de la animación
     requestAnimationFrame(update);
 }
 
 // Iniciar la animación
 update();
+
+
+
